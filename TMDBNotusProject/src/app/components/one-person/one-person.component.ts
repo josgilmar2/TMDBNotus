f3ce901d6b6d503327821ae58765915dde3824f6
Jose Ignacio Rivas Fuentes <rivas.fujos20@triana.salesianos.edu>
2022-11-03 09:00:02 +0100

    add authentication

---
+++ TMDBNotusProject/src/app/components/one-person/one-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-one-person',
+  templateUrl: './one-person.component.html',
+  styleUrls: ['./one-person.component.css']
+})
+export class OnePersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
