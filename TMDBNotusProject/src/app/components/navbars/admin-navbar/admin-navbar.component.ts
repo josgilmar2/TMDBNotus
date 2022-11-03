4fc64b85319edc0d3441e1a06cd41694104cb990
josgilmar2 <josgilmar2@gmail.com>
2022-11-03 17:03:36 +0100

    bugs fixed

--- TMDBNotusProject/src/app/components/navbars/admin-navbar/admin-navbar.component.ts
+++ TMDBNotusProject/src/app/components/navbars/admin-navbar/admin-navbar.component.ts
@@ -1,18 +1,14 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-
 @Component({
   selector: "app-admin-navbar",
   templateUrl: "./admin-navbar.component.html",
 })
 export class AdminNavbarComponent implements OnInit {
 
-  puta: ActivatedRoute = {} as ActivatedRoute;
+  ruta: string = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor() {}
 
   ngOnInit(): void {
-  this.puta = this.route.children[1]
-  console.log(this.puta);
   }
 }
