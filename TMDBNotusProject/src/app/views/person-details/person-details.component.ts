3cbb29899316cddc89aa6f96b2bfe9e2d8ebbb1e
Jose Ignacio <rivas.fujos20@triana.salesianos.edu>
2022-11-05 18:55:09 +0100

    add all movies to person details

--- TMDBNotusProject/src/app/views/person-details/person-details.component.ts
+++ TMDBNotusProject/src/app/views/person-details/person-details.component.ts
@@ -1,46 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PersonDetailsResponse } from 'src/app/models/interfaces/person-details.interface';
-import { Person, KnownFor } from 'src/app/models/interfaces/person.interface';
-import { PersonService } from 'src/app/services/person.service';
-import { environment } from 'src/environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Cast, MovieCreditsResponse } from "src/app/models/interfaces/movie-credits.interface";
+import { PersonDetailsResponse } from "src/app/models/interfaces/person-details.interface";
+import { Person, KnownFor } from "src/app/models/interfaces/person.interface";
+import { PersonService } from "src/app/services/person.service";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-person-details',
-  templateUrl: './person-details.component.html',
-  styleUrls: ['./person-details.component.css']
+  selector: "app-person-details",
+  templateUrl: "./person-details.component.html",
+  styleUrls: ["./person-details.component.css"],
 })
 export class PersonDetailsComponent implements OnInit {
-  personList: Person[] = [];
-  person: Person = {} as Person;
-  pelis: KnownFor[] = [];
   pDetail: PersonDetailsResponse = {} as PersonDetailsResponse;
   page = 1;
-  id = '';
+  id = "";
+  movieList: Cast[] = [];
 
-  constructor(private personService: PersonService, private route: ActivatedRoute, public router: Router) { }
+  constructor(
+    private personService: PersonService,
+    private route: ActivatedRoute,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')!;
+    this.id = this.route.snapshot.paramMap.get("id")!;
 
-    this.personService.getDetails(this.id).subscribe(response => {
+    this.personService.getDetails(this.id).subscribe((response) => {
       this.pDetail = response;
-   });
+    });
+
+    this.personService.getMovieCredits(this.id).subscribe((resp) =>{
+      this.movieList = resp.cast;
+    })
   }
 
   getImg(p: PersonDetailsResponse) {
     let id = p.profile_path;
-    return (`${environment.apiImgUrl}${id}`);
+    return `${environment.apiImgUrl}${id}`;
   }
 
-  parseGender(gender: number){
-    if(gender == 1 ){
-      return 'Female'
-    }else if(gender == 2){
-      return 'Male'
-    }else{
-      return 'Other'
+  getMovieImg(m: Cast){
+    let id = m.poster_path;
+    return (`https://www.themoviedb.org/t/p/w220_and_h330_face/${m.poster_path}`)
+  }
+
+  parseGender(gender: number) {
+    if (gender == 1) {
+      return "Female";
+    } else if (gender == 2) {
+      return "Male";
+    } else {
+      return "Other";
     }
-      
   }
 }
